3c3266cd8cc9b14ba4e9e68c4b7f849b09cec450
Lukas Lüdke <lukasluedke@web.de>
2022-09-16 23:54:57 +0200

    adding an alternative health check, as retrieving the generated help file doesn't work, if the wsdl is protected with authorization

--- rest-to-soap-unit-helper/app.py
+++ rest-to-soap-unit-helper/app.py
@@ -215,6 +215,11 @@ def refresh():
     return jsonify(False), 400
 
 
+@application.route('/health', methods=['GET'])
+def health():
+    return jsonify({'status': 'ok'}), 200
+
+
 if __name__ == '__main__':
     application.debug = True
     application.run(host='0.0.0.0', port=5001)
